1b131c2219cbed37a151caf8f311b699575308e9
klein-thibault <klein.thibault@gmail.com>
2019-05-13 22:25:41 -0400

    Removed army from relic response

--- Sources/RoasterHammer-Shared/Models/Responses/RelicResponse.swift
+++ Sources/RoasterHammer-Shared/Models/Responses/RelicResponse.swift
@@ -16,18 +16,15 @@ public struct RelicResponse: Codable {
     public let id: Int
     public let name: String
     public let description: String
-    public let army: ArmyResponse
     public let weapon: WeaponResponse?
     public let keywords: [KeywordName]
 
     public init(relicDTO: RelicDTO,
-                army: ArmyResponse,
                 weapon: WeaponResponse?,
                 keywords: [KeywordName]) {
         self.id = relicDTO.id
         self.name = relicDTO.name
         self.description = relicDTO.description
-        self.army = army
         self.weapon = weapon
         self.keywords = keywords
     }
